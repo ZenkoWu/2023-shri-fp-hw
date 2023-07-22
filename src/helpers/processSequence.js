02417857d8b3d4be71beb0410876906676c2466d
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-07-23 02:09:47 +0300

    Update processSequence.js

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,60 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import * as R from 'ramda'
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const URLS = {
+    NUMBERS: 'https://api.tech/numbers/base',
+    ANIMALS: 'https://animals.tech/'
+}
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const isLessThan10 = R.lt(R.__, 10)
+const isGreaterThan2 = R.gt(R.__, 2)
+const isValidStr = R.test(/^[0-9]+(\.[0-9]+)?$/)
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const isValidLength = R.pipe(R.length, R.both(isLessThan10, isGreaterThan2))
+const validation = R.allPass([isValidLength, isValidStr])
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const toNumber = value => +value
+const round = value => Math.round(value).toFixed(0)
+const toSquare = value => value ** 2
+const getRemainder = value => value % 3
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const convertToBinary = (id) => api.get(URLS.NUMBERS, {from: 10, to: 2, number: id })
+const getAnimal = (id) => api.get(URLS.ANIMALS+id, {})
+const getResult = R.prop('result')
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
+    const tapWriteLog = R.tap(writeLog)
+
+    const runChain = R.pipe(
+        toNumber,
+        round,
+        tapWriteLog,
+        convertToBinary,
+        R.andThen(getResult),
+        R.andThen(tapWriteLog),
+        R.andThen(R.length),
+        R.andThen(tapWriteLog),
+        R.andThen(toSquare),
+        R.andThen(tapWriteLog),
+        R.andThen(getRemainder),
+        R.andThen(tapWriteLog),
+        R.andThen(getAnimal),
+        R.andThen(getResult),
+        R.andThen(handleSuccess),
+        R.otherwise(handleError)
+    )
+
+    const onInvalidInput = R.partial(handleError, ['ValidationError'])
+    const runValidation = R.ifElse(validation, runChain, onInvalidInput)
+
+    const runCode = R.pipe(tapWriteLog, runValidation)
+
+    runCode(value)
+}
 
 export default processSequence;
